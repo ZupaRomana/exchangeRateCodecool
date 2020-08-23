222ae42530f6f71b4b3255bf3e96086d6b66e15d
Roman <adam.romanowski@ready4s.com>
2020-08-23 03:29:43 +0200

    Implement generating chart whenever currencies selection changes

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
 const apiUrl = 'https://api.exchangeratesapi.io';
 const currencies = [];
-const mainContainer = document.querySelector('.container');
-const selectElement = document.querySelector('.select');
+const ctx = document.querySelector('#chart').getContext('2d');
+const selectBaseElement = document.querySelector('.select-base');
+const selectSymbolElement = document.querySelector('.select-symbol');
+let chart;
 
 (() => {
     fetch(`${apiUrl}/latest`)
         .then(response => response.json())
         .then(response => {
             currencies.push(response.base, ...Object.keys(response.rates));
-            generateRows(response);
             createSelectionOptions();
-            selectElement.value = response.base;
+            selectBaseElement.value = response.base;
+            selectSymbolElement.value = "PLN";
+            reFetchData();
         })
 })();
 
 function createSelectionOptions() {
     let selectionOptionsHtml = '';
     currencies.forEach(currency => selectionOptionsHtml += `<option>${currency}</option>`);
-    selectElement.innerHTML = selectionOptionsHtml;
-    selectElement.addEventListener('change', evt => {
-        fetch(`${apiUrl}/latest?base=${evt.target.value}`)
-            .then(response => response.json())
-            .then(response => generateRows(response))
-    });
+    selectBaseElement.innerHTML = selectionOptionsHtml;
+    selectSymbolElement.innerHTML = selectionOptionsHtml;
+    [selectBaseElement, selectSymbolElement].forEach(element => element.addEventListener('change', () => reFetchData()));
 }
 
-function generateRows(response) {
-    let mainContent = '';
-    for (const property in response.rates) {
-        if (response.rates.hasOwnProperty(property)) {
-            const row = `1${response.base} = ${response.rates[property]}${property}`;
-            const rowHtml = `<p>${row}</p>`;
-            mainContent += rowHtml;
-        }
+function reFetchData() {
+    fetch(`${apiUrl}/history?start_at=2020-01-01&end_at=2020-08-01&base=${selectBaseElement.value}&symbols=${selectSymbolElement.value}`)
+        .then(response => response.json())
+        .then(response => generateChart(response))
+}
+
+function generateChart(response) {
+    if (!!chart) {
+        chart.destroy();
     }
-    mainContainer.innerHTML = mainContent;
+    const dates = Object.keys(response.rates);
+    dates.sort();
+    const data = [];
+    dates.forEach(date => data.push(response.rates[date][selectSymbolElement.value]));
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates, // dates
+            datasets: [{
+                label: selectSymbolElement.value, // currency symbol
+                data: data, // rates
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
 }
